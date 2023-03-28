6b88f3080ade2683a67e7b9fd4065ba37cf44a96
TestGeorgeH <george30032002@gmail.com>
2023-03-28 17:52:14 +0400

    CBV for products/views

--- store/products/urls.py
+++ store/products/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
-from products.views import products, basket_add, basket_remove
+from products.views import ProductsListView, basket_add, basket_remove
 
 app_name = 'products'
 
 urlpatterns = [
-    path('', products, name='index'),
+    path('', ProductsListView.as_view(), name='index'),
     # path('category/<int:category_id>', products, name='category'),
-    path('page/<int:page_number>', products, name='paginator'),
-    path('category/<int:category_id>/page/<int:page_number>', products, name='category'),
+    
+    # path('page/<int:page_number>', products, name='paginator'),
+    # path('category/<int:category_id>/page/<int:page_number>', products, name='category'),
+    path('page/<int:page>', ProductsListView.as_view(), name='paginator'),
+    path('category/<int:category_id>/page/<int:page>', ProductsListView.as_view(), name='category'),
     
     path('baskets/add/<int:product_id>/', basket_add, name='basket_add'), # ../products/baskets/add/<product_id>/
     path('baskets/remove/<int:basket_id>/', basket_remove, name='basket_remove'),
--- store/products/views.py
+++ store/products/views.py
@@ -1,33 +1,62 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
 
 from products.models import Product, ProductCategory, Basket
 from users.models import User
 
-def index(request):
-    context = {"title" : "Store"}
-    return render(request, 'products/index.html', context)
-
-def products(request, category_id=None, page_number=1):
-    if category_id:
-        products = Product.objects.filter(category_id=category_id)
-    else:
-        products = Product.objects.all()
-        
-    per_page = 3
-    paginator = Paginator(products, per_page)
-    products_paginator = paginator.page(page_number)
-
-
-    context = {
-        "title" : "Store - Каталог",
-        "category_id" : category_id,
-        "categories" : ProductCategory.objects.all(),
-        "products" : products_paginator,
-    }
+class IndexView(TemplateView):
+    template_name = 'products/index.html'
     
-    return render(request, 'products/products.html', context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Store"
+        return context
+    
+class ProductsListView(ListView):
+    model = Product
+    template_name = 'products/products.html'
+    paginate_by = 3
+    context_object_name = 'products'    
+    
+    def get_queryset(self):
+        queryset = super(ProductsListView, self).get_queryset()
+        category_id = self.kwargs.get('category_id')
+        print(f"qureyset call {queryset.filter(category_id=category_id) if category_id else queryset}")
+        return queryset.filter(category_id=category_id) if category_id else queryset
+
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Store - Каталог"
+        context["category_id"] = self.kwargs.get('category_id')
+        context["categories"] = ProductCategory.objects.all()
+        
+        print(f"context call {context}")
+        return context
+    
+    
+# def products(request, category_id=None, page_number=1):
+#     if category_id:
+#         products = Product.objects.filter(category_id=category_id)
+#     else:
+#         products = Product.objects.all()
+        
+#     per_page = 3
+#     paginator = Paginator(products, per_page)
+#     products_paginator = paginator.page(page_number)
+
+
+#     context = {
+#         "title" : "Store - Каталог",
+#         "category_id" : category_id,
+#         "categories" : ProductCategory.objects.all(),
+#         "products" : products_paginator,
+#     }
+    
+#     return render(request, 'products/products.html', context)
 
 
 @login_required
