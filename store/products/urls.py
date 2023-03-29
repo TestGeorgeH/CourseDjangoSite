111553d6e60152bddea63002223c236b1727031c
TestGeorgeH <george30032002@gmail.com>
2023-03-29 17:48:40 +0400

    CBV for users/views registration and profile

--- store/products/urls.py
+++ store/products/urls.py
@@ -5,14 +5,9 @@ app_name = 'products'
 
 urlpatterns = [
     path('', ProductsListView.as_view(), name='index'),
-    # path('category/<int:category_id>', products, name='category'),
-    
-    # path('page/<int:page_number>', products, name='paginator'),
-    # path('category/<int:category_id>/page/<int:page_number>', products, name='category'),
     path('page/<int:page>', ProductsListView.as_view(), name='paginator'),
     path('category/<int:category_id>/page/<int:page>', ProductsListView.as_view(), name='category'),
     
     path('baskets/add/<int:product_id>/', basket_add, name='basket_add'), # ../products/baskets/add/<product_id>/
     path('baskets/remove/<int:basket_id>/', basket_remove, name='basket_remove'),
-
 ]
--- store/products/views.py
+++ store/products/views.py
@@ -36,28 +36,6 @@ class ProductsListView(ListView):
         
         print(f"context call {context}")
         return context
-    
-    
-# def products(request, category_id=None, page_number=1):
-#     if category_id:
-#         products = Product.objects.filter(category_id=category_id)
-#     else:
-#         products = Product.objects.all()
-        
-#     per_page = 3
-#     paginator = Paginator(products, per_page)
-#     products_paginator = paginator.page(page_number)
-
-
-#     context = {
-#         "title" : "Store - Каталог",
-#         "category_id" : category_id,
-#         "categories" : ProductCategory.objects.all(),
-#         "products" : products_paginator,
-#     }
-    
-#     return render(request, 'products/products.html', context)
-
 
 @login_required
 def basket_add(request, product_id):
