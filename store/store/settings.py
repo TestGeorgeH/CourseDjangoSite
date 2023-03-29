40809fcc9766336d446db28396d7a95f1948cc11
TestGeorgeH <george30032002@gmail.com>
2023-03-30 00:05:36 +0400

    CBV for users/views login and logout

--- store/store/settings.py
+++ store/store/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'products',
     'users',
 ]
@@ -140,4 +140,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
 
 LOGIN_URL = '/users/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
