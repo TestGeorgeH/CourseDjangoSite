927fb0b5f9b5d8b5d914dc4a1b308ab84480a7ca
TestGeorgeH <george30032002@gmail.com>
2023-04-22 17:15:44 +0400

    + API pagination

--- store/products/serializers.py
+++ store/products/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from products.models import Product
 
 class ProductSerializers(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(slug_field='name', read_only=True)
+
     class Meta:
         model = Product
         fields = ('id', 'name', 'description', 'price', 'quantity', 'image', 'category')
